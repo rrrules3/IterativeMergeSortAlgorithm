458cfb0b855765deca135fa0beb28916ac137b6b
Rohan <rohan.rajesh1205@gmail.com>
2023-03-29 22:49:00 -0400

    Changes
    A few changes to make it more easier to read

--- src/main/java/com/mycompany/IterativeMergeSort/IterativeMergeSort.java
+++ src/main/java/com/mycompany/IterativeMergeSort/IterativeMergeSort.java
@@ -8,10 +8,9 @@ package com.mycompany.IterativeMergeSort;
  *
  * @author Rohan
  */
-import java.util.ArrayList;
+//Importing all packages needed for the code
 import java.util.Scanner;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 public class IterativeMergeSort {
      void printArray(int[] arr) {
     //This method is for the sole purpose of printing out the array
@@ -32,7 +31,7 @@ public class IterativeMergeSort {
          int[] left = new int[mid];
          for(int i = 0; i < mid; i++){
             left[i] = test[i];
-         }
+         } //takes the remaining values to the right of the midpoint and puts it in a different array
          int[] right = new int[test.length - mid];
          for(int i = mid; i < test.length; i++){
             right[i - mid] = test[i];
@@ -42,7 +41,9 @@ public class IterativeMergeSort {
          int a = 0;
          int b = 0;
          int c = 0;
+         //This is where the assigning of values to the array happens!
          while(a < left.length && b < right.length){
+             //if the left side value < right side value, it assigns the left side value to the original, otherwise it assigns the right side value to the original array
             if(left[a] < right[b]){
                test[c] = left[a];
                a++;
@@ -51,7 +52,7 @@ public class IterativeMergeSort {
                b++;
             }
             c++;
-         }
+         } //Once one side of the array is done, it starts using the rest of the values found in the other side to populate it
           while(a < left.length)
             {
                 test[c] = left[a];
@@ -67,25 +68,31 @@ public class IterativeMergeSort {
       }
    }
     public static void main(String[] args)
-    {
+    {   //Initializes Scanner
         Scanner scan = new Scanner(System.in);
         System.out.println("how many elements?");
+        //user inputs the amount of elements they want in the array
         int input = scan.nextInt();
         Random randomize = new Random();
         int[] testingArray = new int[input];
+        //assigns random numbers to fill the array
         for (int i = 0; i < testingArray.length; i++) {
         testingArray[i] = randomize.nextInt(100);
        }
          
         IterativeMergeSort tester = new IterativeMergeSort();
+        //prints out unsorted array
         System.out.println("Unsorted:");
         tester.printArray(testingArray);
+        //times the algorithm in nanoseconds
         double start = System.nanoTime();
         mergeSortIt(testingArray);
-        System.out.println("Sorted:");
         double end = System.nanoTime();
         double total = end - start;
+        //prints out sorted array
+        System.out.println("Sorted:");
         tester.printArray(testingArray);
+        //This calculates the total amount of nanoseconds that it takes for the program to run
         System.out.println("It took " + total + " nanoseconds.");
     }
 }
